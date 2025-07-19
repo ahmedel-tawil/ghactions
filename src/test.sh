9dbb03a4ef0e3a74b63da37d805aab69d8ffc5a3
ahmedabdallah <ahmedeltaweel55@outlook.com>
2025-07-19 06:36:18 +0400

    update test.sh

--- src/test.sh
+++ src/test.sh
@@ -4,14 +4,12 @@
 
 EXPECTED="Hello, Test!"
 
-OUTPUT=$(node -e "console.log(require('./src/app)('Test)")
+OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
 
-if ["$OUTPUT" == "$EXPECTED"]; then
-  echo  "test passed"
-
-exit 0
+if [ "$OUTPUT" == "$EXPECTED" ]; then
+  echo "test passed"
+  exit 0
 else
   echo "Test failed"
-exit 1
-
+  exit 1
 fi
